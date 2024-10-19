6e942b4165c4122c4e601ec7352d30ff04753845
HarryDA31 <henryh.delgadoalonzo@gmail.com>
2024-10-19 13:51:10 -0500

    Agregado: Modificaciones en app.js para escuchar en 0.0.0.0 y otros ajustes

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ const express = require('@braitsch/express');
 
 const app = express();
 
+// Configurar el puerto
+const PORT = process.env.PORT || 3000;
+
+// Iniciar Express
 express.log('./logs');
-
 express.http(app);
-
 express.init(__dirname, app, 'node-login', true);
 
-express.start(app);
+// Asegurarse de que la aplicación escuche en todas las interfaces
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
